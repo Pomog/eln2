aaf342819cb42d663246148fa5f1d1cb9c9ef4a2
Pomog <thoryur@gmail.com>
2023-09-02 23:10:55 +0300

    Initial commit

---
+++ src/main/java/com/ElnMarvin/elnV2/ElnV2Application.java
@@ -0,0 +1,13 @@
+package com.ElnMarvin.elnV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElnV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElnV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ElnMarvin/elnV2/ElnV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ElnMarvin.elnV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElnV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

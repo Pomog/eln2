3f8563a144f00a2f61b0e004f836fa0989b99f89
Pomog <thoryur@gmail.com>
2023-09-06 20:10:54 +0300

    transfer from Marvin to CDK

--- src/main/java/com/ElnMarvin/elnV2/MVCTest1.java
+++ src/main/java/com/ElnMarvin/elnV2/MVCTest1.java
@@ -1,12 +1,19 @@
 package com.ElnMarvin.elnV2;
 
 import com.ElnMarvin.elnV2.domain.Person;
+import org.openscience.cdk.AtomContainer;
+import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.io.MDLV2000Reader;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,5 +49,27 @@ public class MVCTest1 {
 
         model.addAttribute("people", people);
         return "bootstrapTest";
+    }
+
+    @GetMapping ("/getStructure")
+    public String getStructure(Model model) {
+        return "getStructure";
+    }
+
+    @PostMapping("/submitMolsource")
+    public String submitMolsource(@RequestParam("molsource") String molsource, Model model) {
+        System.out.println(molsource);
+
+        MDLV2000Reader mdlv2000Reader = new MDLV2000Reader(new StringReader(molsource));
+        IAtomContainer molecule;
+        try {
+            molecule = mdlv2000Reader.read(new AtomContainer());
+        } catch (CDKException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(molecule.getAtomCount());
+
+        // Redirect to a success page or another view
+        return "sucsessGetMol"; // Create a "success.html" view for displaying a success message
     }
 }
